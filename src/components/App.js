7b8d663533d3b9b45b334b8469cd7b64d6e44228
anapairo <ana.pairo2@gmail.com>
2022-10-19 02:31:57 -0300

    SignUp screen design

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "../common/GlobalStyles";
 
+import SignUp from "./SignUp/signUp";
+
 export default function App() {
   return (
     <>
@@ -10,6 +12,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<> Hello Word </>} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
