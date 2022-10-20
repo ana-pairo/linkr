fa9f9213db8e71bbe84171a28e9c8e554d617aff
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-20 18:54:39 -0300

    Implements Post component

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,10 +1,12 @@
 import { Container } from "../common/Container/ContainerStyle";
 import Header from "../common/Header/Header";
+import Post from "../common/Post/Post";
 
 export default function Tester() {
   return (
     <Container>
       <Header />
+      <Post></Post>
     </Container>
   );
 }
