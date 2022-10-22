ae2f81aced14450b9e2125aa11f3926dafa9062c
igorFrotte <igorfrotte@gmail.com>
2022-10-22 12:31:21 -0300

    Img CreatePost

--- src/common/CreatePost/CreatePost.js
+++ src/common/CreatePost/CreatePost.js
@@ -1,10 +1,30 @@
-import { CreatePostBox } from "./CreatePostStyle";
+import { useContext } from "react";
+import { CreatePostBox, RightHandleBar, LeftHandleBar } from "./CreatePostStyle";
+import PictureContext from "../../contexts/PictureContext";
+import UserContext from "../../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
 
-export default function CreatePost( { page } ) {
+export default function CreatePost() {
+  const { userPicture } = useContext(PictureContext); 
+  const { setUserInfo } = useContext(UserContext); 
+  const navigate = useNavigate();
+
+  function redirect () {
+    setUserInfo({
+      username: JSON.parse(localStorage.getItem("Linkr")).username,
+      picture: userPicture
+    }); 
+    navigate("/user/"+ JSON.parse(localStorage.getItem("Linkr")).userId);
+  }
 
   return (
     <CreatePostBox> 
-        Vai fircar bom... Confia!
+      <LeftHandleBar>
+        <img onClick={redirect} src={userPicture} />
+      </LeftHandleBar>
+      <RightHandleBar>
+        Var dar bom... Confia!
+      </RightHandleBar>
     </CreatePostBox>
   );
 }
